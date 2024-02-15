c17ede6216d5dd074ee8119f89ec2af6ecf4a2a6
abstract-point <diparon@yandex.ru>
2024-02-15 23:00:47 +0300

    Fix linter

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,12 +34,12 @@ class AuthenticatedSessionController extends Controller
 
         $to_name = 'Receiver';
         $to_email = 'receiver@example.com';
-        $data = array('name'=>"Ogbonna Vitalis(sender)", "body" => "A test mail");
+        $data = array('name' => "Ogbonna Vitalis(sender)", "body" => "A test mail");
 
-        Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) {
+        Mail::send('emails.mail', $data, function ($message) use ($to_name, $to_email) {
             $message->to($to_email, $to_name)
                     ->subject('Laravel Test Mail');
-            $message->from('sender@example.com','Test Mail');
+            $message->from('sender@example.com', 'Test Mail');
         });
 
         return redirect()->intended(RouteServiceProvider::HOME);
