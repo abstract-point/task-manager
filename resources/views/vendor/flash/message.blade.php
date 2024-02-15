593221db5d875febb361b3c383b42fd5af9ddbd6
abstract-point <diparon@yandex.ru>
2024-02-15 23:00:19 +0300

    Update flash messages style to Tailwind

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,27 +1,12 @@
 @foreach (session('flash_notification', collect())->toArray() as $message)
-    @if ($message['overlay'])
-        @include('flash::modal', [
-            'modalClass' => 'flash-modal',
-            'title'      => $message['title'],
-            'body'       => $message['message']
-        ])
-    @else
-        <div class="alert
-                    alert-{{ $message['level'] }}
-                    {{ $message['important'] ? 'alert-important' : '' }}"
-                    role="alert"
-        >
-            @if ($message['important'])
-                <button type="button"
-                        class="close"
-                        data-dismiss="alert"
-                        aria-hidden="true"
-                >&times;</button>
-            @endif
-
-            {!! $message['message'] !!}
-        </div>
-    @endif
+<div class="{{ $message['level'] === 'info' ? 'bg-blue-100 border-blue-400 text-blue-700' : '' }}
+            {{ $message['level'] === 'success' ? 'bg-green-100 border-green-400 text-green-700' : '' }}
+            {{ $message['level'] === 'danger' ? 'bg-red-100 border-red-400 text-red-700' : '' }}
+            {{ $message['level'] === 'warning' ? 'bg-yellow-100 border-yellow-400 text-yellow-700' : '' }}
+            px-4 py-3 rounded relative" role="alert">
+  <strong class="font-bold">{{ Str::title($message['level']) }} alert!</strong>
+  <span class="block sm:inline">{{ $message['message'] }}</span>
+</div>
 @endforeach
 
 {{ session()->forget('flash_notification') }}
