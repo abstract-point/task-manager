84b3340b241c284644458096353d0c0e1f898955
abstract-point <diparon@yandex.ru>
2024-02-22 17:50:15 +0300

    Add Task statuses Request

---
+++ app/Http/Requests/TaskStatusRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\TaskStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TaskStatusRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:30',
+                'min:3',
+                Rule::unique('task_statuses', 'name')->ignore($this->task_status)
+            ],
+        ];
+    }
+}
