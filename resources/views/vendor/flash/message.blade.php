82d456b29485014c163cbb4d30e03bd9aaef29df
abstract-point <diparon@yandex.ru>
2024-02-22 17:48:32 +0300

    Fix flash messages view style

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,12 +1,12 @@
 @foreach (session('flash_notification', collect())->toArray() as $message)
-<div class="{{ $message['level'] === 'info' ? 'bg-blue-100 border-blue-400 text-blue-700' : '' }}
-            {{ $message['level'] === 'success' ? 'bg-green-100 border-green-400 text-green-700' : '' }}
-            {{ $message['level'] === 'danger' ? 'bg-red-100 border-red-400 text-red-700' : '' }}
-            {{ $message['level'] === 'warning' ? 'bg-yellow-100 border-yellow-400 text-yellow-700' : '' }}
-            px-4 py-3 rounded relative" role="alert">
-  <strong class="font-bold">{{ Str::title($message['level']) }} alert!</strong>
-  <span class="block sm:inline">{{ $message['message'] }}</span>
-</div>
+    <div class="{{ $message['level'] === 'info' ? 'border-2 dark:border-blue-400 dark:bg-gray-900 dark:text-blue-400' : '' }}
+                {{ $message['level'] === 'success' ? 'border-2 dark:bg-green-100 dark:border-green-400 dark:text-green-700' : '' }}
+                {{ $message['level'] === 'danger' ? 'border-2 dark:bg-red-100 dark:border-red-400 dark:text-red-700' : '' }}
+                {{ $message['level'] === 'warning' ? 'border-2 dark:bg-yellow-100 dark:border-yellow-400 dark:text-yellow-700' : '' }}
+                py-4 px-4" role="alert">
+      <strong class="font-bold">{{ Str::title($message['level']) }} alert!</strong>
+      <span class="block sm:inline">{{ $message['message'] }}</span>
+    </div>
 @endforeach
 
 {{ session()->forget('flash_notification') }}
