861618516ee5f94f107eef86f63a8edc83d273f1
abstract-point <diparon@yandex.ru>
2024-02-22 20:43:05 +0300

    Add en and ru locales

---
+++ lang/ru/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records. Логин или пароль не верные',
+    'password' => 'Неверный пароль',
+    'throttle' => 'Слишком много попыток. Попробуйте снова через :seconds секунд.',
+
+];
