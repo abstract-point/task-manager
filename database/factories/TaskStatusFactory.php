62ec992bb3b864e2312c02c5b10af2b1881f26ad
abstract-point <diparon@yandex.ru>
2024-02-25 18:08:21 +0300

    Fix TaskStatusFactory

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -19,7 +19,7 @@ class TaskStatusFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->word,
+            'name' => Str::random(5),
         ];
     }
 }
