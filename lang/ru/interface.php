afc30d76b41416908a3ce37101b6b13a7930be89
abstract-point <diparon@yandex.ru>
2024-02-26 21:56:52 +0300

    Add i18n for Tasks interface

---
+++ lang/ru/interface.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'task_statuses' => [
+        'index' => [
+            'title' => 'Статусы',
+            'table' => [
+                'id' => 'ID',
+                'name' => 'Имя',
+                'actions' => 'Действия'
+            ],
+            'button' => 'Новый статус',
+        ]
+    ],
+    'tasks' => [
+        'index' => [
+            'title' => 'Задачи',
+            'table' => [
+                'id' => 'ID',
+                'status' => 'Статус',
+                'name' => 'Имя',
+                'author' => 'Автор',
+                'performer' => 'Исполнитель',
+                'created_at' => 'Дата создания',
+                'actions' => 'Действия'
+            ],
+            'button' => 'Новая задача',
+        ],
+        'create' => [
+            'title' => 'Создать задачу',
+            'button' => 'Создать',
+        ],
+        'edit' => [
+            'title' => 'Изменение задачи',
+            'button' => 'Обновить',
+        ],
+        'form' => [
+            'name' => 'Имя',
+            'description' => 'Описание',
+            'status' => 'Статус',
+            'performer' => 'Исполнитель',
+            'labels' => 'Метки',
+        ],
+    ]
+];
