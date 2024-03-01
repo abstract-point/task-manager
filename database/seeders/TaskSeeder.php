863a4f5fd30d5446fdcb927181217968f22c8e80
abstract-point <diparon@yandex.ru>
2024-03-01 22:12:09 +0300

    Refactor Task Seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Label;
+use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,12 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tasks = Task::factory(10)->create();
+        $labels = Label::factory(4)->create();
+
+        foreach ($tasks as $task) {
+            $labelsCount = rand(1, 4);
+            $task->labels()->attach($labels->random($labelsCount));
+        }
     }
 }
