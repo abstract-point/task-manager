d609741197d0ec49952ee3d660cd100279d0f974
abstract-point <diparon@yandex.ru>
2024-03-02 15:56:45 +0300

    Fix linter

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreLabelRequest;
 use App\Http\Requests\UpdateLabelRequest;
 use App\Models\Label;
+
 use function PHPUnit\Framework\isEmpty;
 
 class LabelController extends Controller
