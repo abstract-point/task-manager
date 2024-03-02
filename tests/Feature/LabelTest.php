4958c39865b0db879f5123a193c12b1e5ad1fe40
abstract-point <diparon@yandex.ru>
2024-03-02 15:36:20 +0300

    Add tests for Label

---
+++ tests/Feature/LabelTest.php
@@ -0,0 +1,116 @@
+<?php
+
+
+use App\Models\Label;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LabelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function testIndex(): void
+    {
+        $this->get(route('labels.index'))
+            ->assertStatus(200);
+    }
+
+    public function testCreateAuthed(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->get(route('labels.create'))
+            ->assertStatus(200);
+    }
+
+    public function testCreateNotAuthed(): void
+    {
+        $this->get(route('labels.create'))
+            ->assertRedirectToRoute('login');
+    }
+
+    public function testEditAuthed(): void
+    {
+        $user = User::factory()->create();
+        $label = Label::factory()->create();
+
+        $this->actingAs($user)
+            ->get(route('labels.edit', [$label]))
+            ->assertStatus(200);
+    }
+
+    public function testEditNotAuthed(): void
+    {
+        $label = Label::factory()->create();
+
+        $this->get(route('labels.edit', [$label]))
+            ->assertRedirectToRoute('login');
+    }
+
+    public function testStoreAuthed(): void
+    {
+        $user = User::factory()->create();
+        $label = Label::factory()->make()->only('name');
+
+        $this->actingAs($user)
+            ->post(route('labels.store'), $label)
+            ->assertRedirectToRoute('labels.index')
+            ->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('labels', $label);
+    }
+
+    public function testStoreNotAuthed(): void
+    {
+        $label = Label::factory()->make()->only('name');
+
+        $this->post(route('labels.store'), $label)
+            ->assertRedirectToRoute('login');
+    }
+
+    public function testUpdateAuthed(): void
+    {
+        $user = User::factory()->create();
+        $label = Label::factory()->create();
+        $updatedLabel = Label::factory()->make()->only('name');
+
+        $this->actingAs($user)
+            ->patch(route('labels.update', [$label]), $updatedLabel)
+            ->assertRedirectToRoute('labels.index');
+
+        $this->assertDatabaseHas('labels', $updatedLabel);
+    }
+
+    public function testUpdateNotAuthed(): void
+    {
+        $label = Label::factory()->create();
+
+        $this->patch(route('labels.update', [$label]))
+            ->assertRedirectToRoute('login');
+    }
+
+    public function testDestroyAuthed(): void
+    {
+        $user = User::factory()->create();
+        $label = Label::factory()->create();
+
+        $this->actingAs($user)
+            ->delete(route('labels.destroy', [$label]))
+            ->assertRedirectToRoute('labels.index');
+
+        $this->assertDatabaseMissing('labels', $label->only('id'));
+    }
+
+    public function testDestroyNotAuthed(): void
+    {
+        $label = Label::factory()->create();
+
+        $this->delete(route('labels.destroy', [$label]))
+            ->assertRedirectToRoute('login');
+    }
+}
