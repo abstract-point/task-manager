0bbcfa5e1dbf519e72942741b6b0bb5f2f4be553
abstract-point <diparon@yandex.ru>
2024-03-02 15:48:39 +0300

    Add Label Form Requests

--- app/Http/Requests/StoreLabelRequest.php
+++ app/Http/Requests/StoreLabelRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreLabelRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +14,8 @@ class StoreLabelRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'unique:labels', 'string', 'max:30', 'min:3'],
+            'description' => ['nullable', 'string', 'max:255'],
         ];
     }
 }
--- app/Http/Requests/UpdateLabelRequest.php
+++ app/Http/Requests/UpdateLabelRequest.php
@@ -3,17 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateLabelRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -22,7 +15,10 @@ class UpdateLabelRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:30', 'min:3',
+                Rule::unique('labels', 'name')->ignore($this->label)
+            ],
+            'description' => ['nullable', 'string', 'max:255'],
         ];
     }
 }
