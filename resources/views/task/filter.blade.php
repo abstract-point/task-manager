ee79e5a562736297c85e6975ec04e6ef15dd4687
abstract-point <diparon@yandex.ru>
2024-03-04 21:38:41 +0300

    Add filter view

---
+++ resources/views/task/filter.blade.php
@@ -0,0 +1,57 @@
+<form action="{{route('tasks.index')}}">
+
+    <!-- Inputs -->
+    <div class="flex flex-row">
+        <div class="mr-4">
+            <x-selector-input id="status_id" class="" name="filter[status_id]">
+                <option value="">
+                    {{ 'Статус' }}
+                </option>
+                @foreach($taskStatuses as $status)
+                    <option
+                        value="{{ $status->id }}" {{ ($filterData['status_id'] ?? null) == $status->id ? 'selected' : '' }}>
+                        {{ $status->name }}
+                    </option>
+                @endforeach
+            </x-selector-input>
+            <x-input-error :messages="$errors->get('filter[status_id]')" class="mt-2"/>
+        </div>
+        <div class="mr-4">
+            <x-selector-input id="created_by_id" class="" name="filter[created_by_id]">
+                <option value="">
+                    {{ 'Автор' }}
+                </option>
+                @foreach($users as $user)
+                    <option
+                        value="{{ $user->id }}" {{ ($filterData['created_by_id'] ?? null) == $user->id ? 'selected' : '' }}>
+                        {{ $user->name }}
+                    </option>
+                @endforeach
+            </x-selector-input>
+            <x-input-error :messages="$errors->get('filter[created_by_id]')" class="mt-2"/>
+        </div>
+        <div class="mr-4">
+            <x-selector-input id="assigned_to_id" class="" name="filter[assigned_to_id]">
+                <option value="">
+                    {{ 'Исполнитель' }}
+                </option>
+                @foreach($performers as $performer)
+                    <option
+                        value="{{ $performer->id }}" {{ ($filterData['assigned_to_id'] ?? null) == $performer->id ? 'selected' : '' }}>
+                        {{ $performer->name }}
+                    </option>
+                @endforeach
+            </x-selector-input>
+            <x-input-error :messages="$errors->get('filter[assigned_to_id]')" class="mt-2"/>
+        </div>
+
+        <!-- Button -->
+        <div class="flex">
+            <x-primary-button class="">
+                {{ __('interface.tasks.filter.button') }}
+            </x-primary-button>
+        </div>
+    </div>
+
+
+</form>
