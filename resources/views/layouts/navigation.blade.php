b10caede87165ab28f537ea0d1fb0afbc4e84ec4
abstract-point <diparon@yandex.ru>
2024-03-10 16:31:35 +0300

    Fix logout button for Hexlet check

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,17 @@
                 <x-nav-link :href="route('labels.index')" :active="request()->routeIs('labels.*')">
                     {{ __('interface.navigation.labels') }}
                 </x-nav-link>
+                @auth
+                <form method="POST" action="{{ route('logout') }}" class="inline-flex items-center">
+                    @csrf
+
+                    <x-nav-link :href="route('logout')"
+                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                        {{ __('interface.navigation.logout') }}
+                    </x-nav-link>
+                </form>
+                @endauth
             </div>
 
             <!-- Settings Dropdown -->
@@ -48,15 +59,15 @@
                             </x-dropdown-link>
 
                             <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
+{{--                            <form method="POST" action="{{ route('logout') }}">--}}
+{{--                                @csrf--}}
 
-                                <x-dropdown-link :href="route('logout')"
-                                                 onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('interface.navigation.logout') }}
-                                </x-dropdown-link>
-                            </form>
+{{--                                <x-dropdown-link :href="route('logout')"--}}
+{{--                                                 onclick="event.preventDefault();--}}
+{{--                                                this.closest('form').submit();">--}}
+{{--                                    {{ __('interface.navigation.logout') }}--}}
+{{--                                </x-dropdown-link>--}}
+{{--                            </form>--}}
                         </x-slot>
                     </x-dropdown>
                 </div>
