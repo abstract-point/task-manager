a18689d31205d0ab73a69718c1228e96bfd6e7c9
abstract-point <diparon@yandex.ru>
2024-08-13 17:06:23 +0300

    Improve routes code style

--- routes/web.php
+++ routes/web.php
@@ -6,44 +6,32 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::prefix('')->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    })->name('index');
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
+Route::get('/', function () {
+    return view('welcome');
+})->name('index');
 
-    require __DIR__ . '/auth.php';
-
-    Route::middleware(['auth'])->group(function () {
-        Route::resource('task_statuses', TaskStatusController::class)
-            ->parameters(['task_statuses' => 'status'])
-            ->except(['index']);
-        Route::resource('tasks', TaskController::class)
-            ->except(['index', 'show']);
-        Route::resource('labels', LabelController::class)
-            ->except(['index', 'show']);
-    });
-
-    Route::resource('task_statuses', TaskStatusController::class)
-        ->only(['index']);
-    Route::resource('tasks', TaskController::class)
-        ->only(['index', 'show']);
-    Route::resource('labels', LabelController::class)
-        ->only(['index']);
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__ . '/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('task_statuses', TaskStatusController::class)
+        ->parameters(['task_statuses' => 'status'])
+        ->except(['index']);
+    Route::resource('tasks', TaskController::class)
+        ->except(['index', 'show']);
+    Route::resource('labels', LabelController::class)
+        ->except(['index', 'show']);
+});
+
+Route::resource('task_statuses', TaskStatusController::class)
+    ->only(['index']);
+Route::resource('tasks', TaskController::class)
+    ->only(['index', 'show']);
+Route::resource('labels', LabelController::class)
+    ->only(['index']);
